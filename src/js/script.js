8862c7e52a68704bc2eefdbb8923ef6cf447f1ac
Shujinko53 <shujinko53@yandex.ru>
2021-07-16 21:21:40 +0300

    The overlay has been added

--- src/js/script.js
+++ src/js/script.js
@@ -2,17 +2,26 @@ window.addEventListener('DOMContentLoaded', () => {
     const mainBtn = document.querySelector('.btn'),
           rulesLink = document.querySelector('.footer__link'),
           modal = document.querySelector('.modal'),
-          modalClose = document.querySelector('.modal__close');
+          modalClose = document.querySelector('.modal__close'),
+          overlay = document.querySelector('.overlay');
 
     mainBtn.addEventListener('click', () => {
         console.log('Button clicked');
     })
 
     rulesLink.addEventListener('click', () => {
+        overlay.style.display = "block";
         modal.style.display = "block";
     });
 
     modalClose.addEventListener('click', () => {
         modal.style.display = "none";
+        overlay.style.display = "none";
     });
+
+    window.addEventListener('keydown', (evt) => {
+        if(evt.keyCode == 27) {
+            modal.style.display = "none";
+        }
+    })
 });
